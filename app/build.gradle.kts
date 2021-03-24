d18a76bfbca0339f814cbd76818e33686e69e5b2
WW <13792916787@163,com>
2021-03-25 00:00:46 +0800

    buildSrc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 }
 //Android属性
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(AppConfig.compileSdkVersion)
     defaultConfig {
-        applicationId = "com.ww.mo"
-        minSdkVersion (21)
-        targetSdkVersion (29)
-        versionCode = 1
-        versionName = "1.0"
+        applicationId = AppConfig.applicationId
+        minSdkVersion (AppConfig.minSdkVersion)
+        targetSdkVersion (AppConfig.targetSdkVersion)
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -67,11 +67,11 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:${KotlinConstants.kotlin_version}")
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation(DependenciesConfig.STD_LIB)
+    implementation(DependenciesConfig.KTX_CORE)
+    implementation(DependenciesConfig.APP_COMPAT)
+    implementation(DependenciesConfig.MATERIAL)
+    implementation(DependenciesConfig.CONSTRAINT_LAYOUT)
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
--- buildSrc/src/main/kotlin/KotlinConstants.kt
+++ buildSrc/src/main/kotlin/KotlinConstants.kt
@@ -1,8 +1,47 @@
 //全局常量
-object KotlinConstants{
+object KotlinConstants {
     //Gradle 版本
     const val gradle_version = "4.1.2"
 
     //Kotlin 版本
     const val kotlin_version = "1.3.71"
+}
+
+//应用配置
+object AppConfig {
+    //依赖版本
+    const val compileSdkVersion = 29
+
+    //包名
+    const val applicationId = "com.ww.mo"
+
+    //最小支持SDK
+    const val minSdkVersion = 21
+
+    //当前基于SDK
+    const val targetSdkVersion = 29
+
+    //版本编码
+    const val versionCode = 1
+
+    //版本名称
+    const val versionName = "1.0.0"
+}
+
+//依赖配置
+object DependenciesConfig {
+    //Kotlin基础库
+    const val STD_LIB = "org.jetbrains.kotlin:kotlin-stdlib:${KotlinConstants.kotlin_version}"
+
+    //Kotlin核心库
+    const val KTX_CORE = "androidx.core:core-ktx:1.3.2"
+
+    //Android标准库
+    const val APP_COMPAT = "androidx.appcompat:appcompat:1.2.0"
+
+    const val MATERIAL = "com.google.android.material:material:1.3.0"
+
+    const val CONSTRAINT_LAYOUT ="androidx.constraintlayout:constraintlayout:2.0.4"
+
+
 }
